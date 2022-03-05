1d91539063366fd37003d3e8045e02c2a0906550
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-05 16:32:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+// splicing in c language 
+#include<stdio.h>
+
+int main()
+{
+  //splicing '\'
+  printf("%s\n","you have to dream big.\
+    --APJ Abdul Kalam");
+  printf("%s","you have to dream big."
+    "--APJ Abdul Kalam");
+  return 0;
+}
